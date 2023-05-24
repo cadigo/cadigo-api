43f5d35b8c5d7823daa3d4c2bb1cab8db42fc3b3
Siitha.Raksasawat <sittha.raksasawat@gmail.com>
2023-05-24 23:13:36 +0700

    fix bug

--- config/con-struct.go
+++ config/con-struct.go
@@ -4,7 +4,7 @@ import "cadigo-api/db/mongodb/infrastructure"
 
 type Config struct {
 	Environment string `env:"ENVIRONMENT,required"`
-	Version     int    `env:"VERSION,required"`
+	Version     string `env:"VERSION,required"`
 	infrastructure.MongodbConfig
 	infrastructure.MongodbRepositoryCfg
 }
